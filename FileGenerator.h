3fdfcae1ad3e1d1845ebd7c3ec82d9e236291e3e
ashtonhess <ashton.hess@yahoo.com>
2022-05-05 12:47:42 -0500

    Implementing getters and setters for substringcounter class

--- FileGenerator.h
+++ FileGenerator.h
@@ -4,7 +4,8 @@
 
 #ifndef CODE_FILEGENERATOR_H
 #define CODE_FILEGENERATOR_H
-
+#include <iostream>
+using namespace std;
 
 class FileGenerator {
 
--- SubstringCounter.cpp
+++ SubstringCounter.cpp
@@ -3,3 +3,17 @@
 //
 
 #include "SubstringCounter.h"
+void SubstringCounter::setSearchSubstring(string searchSubstring){
+    search_substring = searchSubstring;
+}
+string SubstringCounter::getSearchSubstring(){
+    return search_substring;
+}
+void SubstringCounter::setSubstringCount(int substringCount){
+    substring_count = substringCount;
+}
+int SubstringCounter::getSubstringCount(){
+    return substring_count;
+}
+
+//int readAndCount();
